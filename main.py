8f7f0a2d9568c631cbadd7d8cb34dc2198ce09e2
thomaoc <thomas.ocuilleanain@ulb.be>
2025-02-03 13:10:07 +0100

    models loaded

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-# Training
+from torchvision.models import resnet50
+from sentence_transformers import SentenceTransformer
+
+caption_encoder = SentenceTransformer("all-MiniLM-L6-v2")
+caption_encoder.eval()
+
+image_encoder = resnet50()
