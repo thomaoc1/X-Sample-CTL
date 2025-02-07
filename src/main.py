545a9c8cb2fef16db895955144af3216df16863b
thomaoc <thomas.ocuilleanain@ulb.be>
2025-02-07 13:58:35 +0100

    logging

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from torchvision.models import resnet50
 from torchvision.transforms import transforms, autoaugment
 from sentence_transformers import SentenceTransformer
 
-from src.util import caption_from_labels
+from util import caption_from_labels
 
 
 def init_data_loader(path: str, batch_size: int = 64) -> DataLoader:
@@ -66,14 +66,17 @@ def train(class_labels: list, batch_size=1024, tau=1, device='cpu'):
     )
 
     similarity_graph = compute_similarity_graph(class_labels, caption_encoder) / tau
+    similarity_graph = similarity_graph.to(device)
 
     optimiser = optim.Adam(
         list(image_encoder.parameters()) + list(head.parameters()),
-        lr=1e-4,
+        lr=7.5e-2,
     )
 
-    epochs = 10
+    epochs = 100
+    losses = []
     for epoch in range(epochs):
+        epoch_loss = 0
         for images, labels in loader:
             # (1) Augment images twice and concat
             augmented_images = torch.concat(
@@ -87,7 +90,7 @@ def train(class_labels: list, batch_size=1024, tau=1, device='cpu'):
             sub_sim_graph = similarity_graph[labels][:, labels]
 
             # (3) Apply column-wise softmax to G
-            softmax_sim_graph = nn.functional.softmax(sub_sim_graph, dim=1).to(device)
+            softmax_sim_graph = nn.functional.softmax(sub_sim_graph, dim=1)
 
             # (4) Forward pass
             backbone_encoding = image_encoder(augmented_images).flatten(1)
@@ -101,8 +104,12 @@ def train(class_labels: list, batch_size=1024, tau=1, device='cpu'):
             optimiser.zero_grad()
             loss.backward()
             optimiser.step()
-            print(loss.item())
 
+            epoch_loss += loss.item()
+
+        avg_loss = epoch_loss / len(loader)
+        losses.append(avg_loss)
+        print(f"Epoch {epoch + 1}/{epochs} - Loss: {avg_loss:.4f}")
 
         torch.save(
             image_encoder.parameters(),
@@ -113,6 +120,8 @@ def train(class_labels: list, batch_size=1024, tau=1, device='cpu'):
             head.parameters(),
             'head_parameters.pt',
         )
+
+    print(losses, flush=True)
 
 
 if __name__ == '__main__':
