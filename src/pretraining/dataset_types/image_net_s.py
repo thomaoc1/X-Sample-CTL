872b03eb451448b0e538148272efabc76fea2484
thomaoc <thomas.ocuilleanain@ulb.be>
2025-02-14 14:23:52 +0100

    ImageNet normalisation

--- src/pretraining/dataset_types/image_net_s.py
+++ src/pretraining/dataset_types/image_net_s.py
@@ -54,7 +54,8 @@ class ImageNetS(ImageFolderDataset):
                     transforms.RandomApply([color_jitter], p=0.8),
                     transforms.RandomGrayscale(p=0.2),
                     transforms.GaussianBlur(kernel_size=kernel_size),
-                    transforms.Lambda(lambd=lambda x: x / 255.0)
+                    transforms.Lambda(lambd=lambda x: x / 255.0),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                 ]
             )
         else:
@@ -62,5 +63,6 @@ class ImageNetS(ImageFolderDataset):
                 [
                     autoaugment.AutoAugment(policy=autoaugment.AutoAugmentPolicy.IMAGENET),
                     transforms.Lambda(lambd=lambda x: x / 255.0),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                 ]
             )
