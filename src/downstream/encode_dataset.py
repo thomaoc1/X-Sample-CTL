2c93789d9e29527d31995ba0b151f4d9e7822105
thomaoc <thomas.ocuilleanain@ulb.be>
2025-02-18 16:48:43 +0100

    doc

--- src/downstream/encode_dataset.py
+++ src/downstream/encode_dataset.py
@@ -21,7 +21,7 @@ def parse_args():
         '-t',
         default=None,
         type=str,
-        help='Path to the dataset to be encoded'
+        help="Path to the dataset to be encoded or choices=['imgnet-s', 'cifar10', 'stl10', 'bgd-ms', 'bgd-mr', 'bgd-nb']"
     )
     return parser.parse_args()
 
